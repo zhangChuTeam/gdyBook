00f58a8d721d4deb606f67233c14e0b83dcc540e
gdyBook <1035215694@qq.com>
2016-11-19 16:50:38 +0800

    first add

--- app/app.js
+++ app/app.js
@@ -1,8 +1,25 @@
-angular.module('projectDemo',['ui.router','angularCSS',
-'projectDemo.homePage',
-'projectDemo.catePage',
-'projectDemo.cartPage',
-'projectDemo.minePage'])
+angular.module('projectDemo',['ui.router'])
 .config(function($stateProvider,$urlRouterProvider){
   $urlRouterProvider.otherwise('/home')
+  $stateProvider
+  .state({
+  	name:'home',
+  	url:'/home',
+  	templateUrl:'app/pages/home/home.html'
+  })
+  .state({
+  	name:'lohas',
+  	url:'/lohas',
+  	templateUrl:'app/pages/lohas/lohas.html'
+  })
+  .state({
+  	name:'community',
+  	url:'/community',
+  	templateUrl:'app/pages/community/community.html'
+  })
+  .state({
+  	name:'my',
+  	url:'/my',
+  	templateUrl:'app/pages/my/my.html'
+  })
 })
