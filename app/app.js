eb7b1358d852665cc6bfc701c1b344639351ce7a
kioim <872768760@qq.com>
2016-11-21 18:04:38 +0800

    del

--- app/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('projectDemo',['ui.router',"angularCSS",
-"homePage",
-"lohasPage",
-"communityPage",
-"myPage"])
-.config(function($stateProvider,$urlRouterProvider){
-  $urlRouterProvider.otherwise('/home')
-})
