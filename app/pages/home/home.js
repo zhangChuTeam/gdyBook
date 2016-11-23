e3b4acac17cfd03b0071dc0009aed5513c1f80c5
xiaoweizi2 <2678842451@qq.com>
2016-11-23 08:37:12 +0800

    home son

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -9,18 +9,77 @@ angular.module('homePage', ["ksSwiper"])
 				controller: 'homeCtrl'
 			})
 			.state({
-				name: 'new',
-				url: '/new',
-				css: 'app/pages/home/new/new.css',
-				templateUrl: 'app/pages/home/new/new.html',
-				controller: function($css) {
-					$css.add('app/pages/home/home.css')
+				name: "new",
+				url: "/new",
+				css: "app/pages/home/new/new.css",
+				templateUrl: "app/pages/home/new/new.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+					$http.get("app/pages/home/json/new.json")
+					.success(function (req){
+						$scope.datas = req.data
+						$scope.lists = req.data.dishes_list				
+					})
 				}
 			})
 			.state({
-				name: 'home.part2',
-				url: '/part2',
-				templateUrl: 'app/pages/home/home.part2/home.part2.html'
+				name: "more",
+				url: "/more",
+				css: "app/pages/home/more/more.css",
+				templateUrl: "app/pages/home/more/more.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+					$http.get("app/pages/home/json/more.json")
+					.success(function (req){
+						$scope.datas = req.data.data
+						$scope.lists = req.data.dishes_list				
+					})
+				}
+			})
+			.state({
+				name: "newPeople",
+				url: "/newPeople",
+				css: "app/pages/home/newPeople/newPeople.css",
+				templateUrl: "app/pages/home/newPeople/newPeople.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+				}
+			})
+			.state({
+				name: "food",
+				url: "/food",
+				css: "app/pages/home/food/food.css",
+				templateUrl: "app/pages/home/food/food.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+				}
+			})
+			.state({
+				name: "guess",
+				url: "/guess",
+				css: "app/pages/home/guess/guess.css",
+				templateUrl: "app/pages/home/guess/guess.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+				}
+			})
+			.state({
+				name: "choose",
+				url: "/choose",
+				css: "app/pages/home/choose/choose.css",
+				templateUrl: "app/pages/home/choose/choose.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+				}
+			})
+			.state({
+				name: "search",
+				url: "/search",
+				css: "app/pages/home/search/search.css",
+				templateUrl: "app/pages/home/search/search.html",
+				controller: function($css, $scope, $http) {
+					$css.add("app/pages/home/home.css");
+				}
 			})
 	})
 	.controller('homeCtrl', function($scope, $http) {
@@ -68,23 +127,29 @@ angular.module('homePage', ["ksSwiper"])
 				}
 				$(".wrap").on("swipeLeft",function (){
 					var lastLeft = parseInt($(this).css("margin-left"));
+					var index = Math.abs(lastLeft/parseInt($(this).width())*3) + 1;
 					var left = parseInt($(".nav span").css("left"))/innerWidth;
 					var wid = parseInt($(".wrap>div").width());
 					if(lastLeft <= -2*wid){
 						return;
-					}					
+					}			
+					$(".plan").on("touchend",function (){
+						return false;
+					});
 					$(".wrap").css({
 						"margin-left":lastLeft - wid,
 						"transition":"margin-left 1s"
 					});
-//					$(".nav span").css({
-//						"left":left + 1.902778 + "rem",
-//						"transition":"left 1s"
-//					})
-//					console.log($(".nav span").css("left"))
+					$(".nav span").css({
+						"left":0.333333 + 1.6*index + "rem",
+						"transition":"left 1s"
+					})
 				})
+				
 				$(".wrap").on("swipeRight",function (){
 					var lastLeft = parseInt($(this).css("margin-left"));
+					var index = Math.abs(lastLeft/parseInt($(this).width())*3)-1;
+					var left = parseInt($(".nav span").css("left"))/innerWidth;
 					var wid = parseInt($(".wrap>div").width());
 					if(lastLeft >= 0){
 						return;
@@ -93,6 +158,10 @@ angular.module('homePage', ["ksSwiper"])
 						"margin-left":lastLeft + wid,
 						"transition":"margin-left 1s"
 					});
+					$(".nav span").css({
+						"left":0.333333 + 1.6*index + "rem",
+						"transition":"left 1s"
+					})
 				})
 			});
 		$http.get("app/pages/home/json/stuff.json")
